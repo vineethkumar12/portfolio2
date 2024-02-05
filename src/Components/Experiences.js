5ec9a014aa481a5d5a57a9844251c801a812f80c
vineethkumar12 <adepuvineethkumarvinni@gmail.com>
2024-02-05 19:06:56 +0530

    updated

--- src/Components/Experiences.js
+++ src/Components/Experiences.js
@@ -1,75 +1,55 @@
-import React from 'react'
-import html from '../assets/html.png' 
-import github from '../assets/github.png'
-import css from '../assets/css.png'
-import react from '../assets/react.png'
-import javascript from '../assets/javascript.png'
-import tailwind from '../assets/tailwind.png' 
-import Tilt from 'react-tilt'
-import './N.css'
-const experience =[
-    { id:1, 
-        nam:"React",
-      src: react,
-    st:"shadow-blue-500",},
-    { id:2, 
-        nam:"javascript",
-    src: javascript, 
-    st:"shadow-yellow-500",},
-    { id:3, 
-        nam:"Tailwind",
-    src: tailwind,
-    st:"shadow-sky-500",},
-    { id:4,
-        nam:"Css",
-    src: css,
-    st:"shadow-blue-500",},
-    { id:5, 
-        nam:"Html",
-    src: html,
-    st:"shadow-orange-500",},
-    { id:6,
-        nam:"Github",
-    src: github, 
-    st:"shadow-gray-500",}
-                                   
-]
-
-
-
-
-
+import React from "react";
+import html from "../assets/html.png";
+import github from "../assets/github.png";
+import css from "../assets/css.png";
+import react from "../assets/react.png";
+import javascript from "../assets/javascript.png";
+import tailwind from "../assets/tailwind.png";
+import Tilt from "react-tilt";
+import "./N.css";
+const experience = [
+  { id: 1, nam: "React", src: react, st: "shadow-blue-500" },
+  { id: 2, nam: "javascript", src: javascript, st: "shadow-yellow-500" },
+  { id: 3, nam: "Tailwind", src: tailwind, st: "shadow-sky-500" },
+  { id: 4, nam: "Css", src: css, st: "shadow-blue-500" },
+  { id: 5, nam: "Html", src: html, st: "shadow-orange-500" },
+  { id: 6, nam: "Github", src: github, st: "shadow-gray-500" },
+];
 
 export const Experiences = () => {
   return (
-    <div id="experience" className='bg-gradient-to-b from-gray-800 to-black text-white-500   w-full text-white md:h-screen'>
-         <div className='flex flex-col justify-center  w-full h-full mx-auto  p-4 max-w-screen-lg'>
-             <div className='mt-5'>
-                 <p className='font-bold border-b-4   border-gray-500  text-4xl w-52'> Experiences</p>
-                 <p className='py-6'> I learned this kind of technologies</p>
-             </div>
-          
-          <Tilt options={{ max : 25 }} >
-           <div className=' grid  text-center py-8  gap-8 px-12 sm:px-0 md:grid-cols-3 sm:grid-cols-2'>
-             
-               {  experience.map((src,i)=>{
-           
-             return (<div key={experience[i].id} className={`  animate shadow-md ${experience[i].st} rounded-lg`}>
-                  <img  src={experience[i].src}  alt="" className='hover:scale-125 w-20 mx-auto h-24 mt-3 rounded-md  cursor-pointer duration-200'/>
-                    <p className='mt-4'>{experience[i].nam}</p>
-              </div>)
-             
-
-})  }    
-
-
-           </div>
-
-           </Tilt>
-
+    <div
+      id="experience"
+      className="bg-gradient-to-b from-gray-800 to-black text-white-500   w-full text-white md:h-screen"
+    >
+      <div className="flex flex-col justify-center  w-full h-full mx-auto  p-4 max-w-screen-lg">
+        <div className="mt-5">
+          <p className="font-bold border-b-4   border-gray-500  text-4xl w-52">
+            Skills
+          </p>
+          <p className="py-6"> I learned this kind of technologies</p>
         </div>
 
-
+        <Tilt options={{ max: 25 }}>
+          <div className=" grid  text-center py-8  gap-8 px-12 sm:px-0 md:grid-cols-3 sm:grid-cols-2">
+            {experience.map((src, i) => {
+              return (
+                <div
+                  key={experience[i].id}
+                  className={`  animate shadow-md ${experience[i].st} rounded-lg`}
+                >
+                  <img
+                    src={experience[i].src}
+                    alt=""
+                    className="hover:scale-125 w-20 mx-auto h-24 mt-3 rounded-md  cursor-pointer duration-200"
+                  />
+                  <p className="mt-4">{experience[i].nam}</p>
+                </div>
+              );
+            })}
+          </div>
+        </Tilt>
+      </div>
     </div>
-  )
-}
+  );
+};
--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,82 +1,92 @@
-import React from 'react'
-import chat from '../assets/portfolio/chat.png'
-import movies1 from '../assets/portfolio/movies1.jpg' 
-import tic from '../assets/portfolio/tic.jpg'
-import face1 from '../assets/portfolio/face1.jpg'
-import robots1 from '../assets/portfolio/robots1.jpg' 
-import stopwatch from '../assets/portfolio/stopwatch.jpg' 
-import Tilt from 'react-tilt'
-export const Portfolio = () => { 
-
-const portfolio =[
-    { id:1,
+import React from "react";
+import chat from "../assets/portfolio/chat.png";
+import movies1 from "../assets/portfolio/movies1.jpg";
+import tic from "../assets/portfolio/tic.jpg";
+import face1 from "../assets/portfolio/face1.jpg";
+import robots1 from "../assets/portfolio/robots1.jpg";
+import stopwatch from "../assets/portfolio/stopwatch.jpg";
+import Tilt from "react-tilt";
+export const Portfolio = () => {
+  const portfolio = [
+    {
+      id: 1,
       src: chat,
-      link1:"https://vineethkumar12.github.io/chatapplication/",
-      name:"Chat Application"
+      link1: "https://vineethkumar12.github.io/chatapplication/",
+      name: "Chat Application",
     },
-    { id:2,
-    src: movies1,
-  link1:"https://vineethkumar12.github.io/Moviesappwithalan/",
-   name:"Moviesapp with Alan"},
-    { id:3,
-    src: tic,
-    link1:"https://vineethkumar12.github.io/tic-toc-toe/",
-    name:"Tic-Toc-Toe"
+    {
+      id: 2,
+      src: movies1,
+      link1: "https://vineethkumar12.github.io/Moviesappwithalan/",
+      name: "Moviesapp with Alan",
     },
-    { id:4,
-    src: face1,
-    link1:"https://vineethkumar12.github.io/facedetectionapp/",
-    name:"Face Detection Application"
-    
-  },
-    { id:5,
-    src: robots1,
-    link1:"https://vineethkumar12.github.io/Robots/",
-    name:"Robots Application"
-    
- },
-    { id:6,
-    src: stopwatch,
-    link1:"https://vineethkumar12.github.io/stopwatch/",
-    name:"Stop Watch"
-  }
-                                   
-]
-
-
+    {
+      id: 3,
+      src: tic,
+      link1: "https://vineethkumar12.github.io/tic-toc-toe/",
+      name: "Tic-Toc-Toe",
+    },
+    {
+      id: 4,
+      src: face1,
+      link1: "https://vineethkumar12.github.io/facedetectionapp/",
+      name: "Face Detection Application",
+    },
+    {
+      id: 5,
+      src: robots1,
+      link1: "https://vineethkumar12.github.io/Robots/",
+      name: "Robots Application",
+    },
+    {
+      id: 6,
+      src: stopwatch,
+      link1: "https://vineethkumar12.github.io/stopwatch/",
+      name: "Stop Watch",
+    },
+  ];
 
   return (
-    <div id="Portfolio" className='bg-gradient-to-b from-black to-gray-800 text-white-500   w-full text-white md:h-screen'> 
-        <div className='flex flex-col justify-center w-full h-full mx-auto  p-4 max-w-screen-lg'>
-              < div className=" mt-12" >
-              <p className="font-bold border-b-4   border-gray-500 w-32  text-4xl "> Portfolio</p> 
-              <p className='py-6'> Check out some of my projects right here</p>
-              </div>
-              
-            <div className=' grid gap-8 px-12 sm:px-0 md:grid-cols-3 sm:grid-cols-2'>
-
-           { portfolio.map(( src,id)=> {
-             
-             return(  <Tilt>
-                <div key={id}  className='shadow-md shadow-gray-500 rounded-lg'>
-                   <a href={src.link1}   > <img src={src.src}  alt="v "  className='hover:scale-105 duration-200 rounded-md cursor-pointer'/></a>
-                     <div className=' flex justify-center items-center'>
-                     <a href={src.link1}  >  <h1 className='  px-6 py-3 duration-150 hover:scale-105'>{src.name}</h1> </a>
-                       
-                        
-                      </div> 
-                </div> 
-              
-            
-                </Tilt>  )
-
-           }) }
-           </div>
-
-
-
+    <div
+      id="Portfolio"
+      className="bg-gradient-to-b from-black to-gray-800 text-white-500   w-full text-white md:h-screen"
+    >
+      <div className="flex flex-col justify-center w-full h-full mx-auto  p-4 max-w-screen-lg">
+        <div className=" mt-12">
+          <p className="font-bold border-b-4   border-gray-500 w-32  text-4xl ">
+            {" "}
+            Projects
+          </p>
+          <p className="py-6"> Check out some of my projects right here</p>
         </div>
 
+        <div className=" grid gap-8 px-12 sm:px-0 md:grid-cols-3 sm:grid-cols-2">
+          {portfolio.map((src, id) => {
+            return (
+              <Tilt>
+                <div key={id} className="shadow-md shadow-gray-500 rounded-lg">
+                  <a href={src.link1}>
+                    {" "}
+                    <img
+                      src={src.src}
+                      alt="v "
+                      className="hover:scale-105 duration-200 rounded-md cursor-pointer"
+                    />
+                  </a>
+                  <div className=" flex justify-center items-center">
+                    <a href={src.link1}>
+                      {" "}
+                      <h1 className="  px-6 py-3 duration-150 hover:scale-105">
+                        {src.name}
+                      </h1>{" "}
+                    </a>
+                  </div>
+                </div>
+              </Tilt>
+            );
+          })}
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
