dc2f50cb0f21f08233a26f2af853153347be6a20
vineethkumar12 <adepuvineethkumarvinni@gmail.com>
2024-02-23 23:12:43 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,30 +1,29 @@
-import  Navbar  from "./Components/Navbar";
-import './App.css' 
-import { Home } from "./Components/Home"; 
+import Navbar from "./Components/Navbar";
+import "./App.css";
+import { Home } from "./Components/Home";
 import { Sociallinks } from "./Components/Sociallinks";
 import { About } from "./Components/About";
 import { Portfolio } from "./Components/Portfolio";
 import { Experiences } from "./Components/Experiences";
 import { Contact } from "./Components/Contact";
+import { Helmet } from "react-helmet";
 
-function App() { 
-  
-
-// const a="grgjkhjgkkgdkhjtgkjrigjjjjjjjjjjjjjjj"
+function App() {
+  // const a="grgjkhjgkkgdkhjtgkjrigjjjjjjjjjjjjjjj"
   return (
-    <div className="App"> 
-   
-<Navbar/> 
+    <div className="App">
+      <Helmet>
+        <meta name="keyword" content="vineeth-kumar-portfolio" />
+      </Helmet>
 
-<Home/>  
-<About/>  
-<Portfolio/> 
-<Experiences/> 
-<Contact/>
-<Sociallinks/>   
+      <Navbar />
 
-
-
+      <Home />
+      <About />
+      <Portfolio />
+      <Experiences />
+      <Contact />
+      <Sociallinks />
     </div>
   );
 }
